b4ac72fc2b0cc57397af05d4227d4ce0368bd255
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-14 03:16:51 -0300

    teste com codigo funcionando

--- script.js
+++ script.js
@@ -2,26 +2,40 @@ function contar(){
     var inicio = document.querySelector('#txtinicio');
     var fim = document.querySelector('#txtfim');
     var passo = document.querySelector('#txtpasso');
-    var res = document.querySelector('#res');
+    var res = document.querySelector('#txtres');
 
-    var inicioValue = inicio.value;
-    var fimValue = fim.value;
-    var passoValue = passo.value;
+    var inicioValue = Number(inicio.value);
+    var fimValue = Number(fim.value);
+    var passoValue = Number(passo.value);
 
-       while( inicioValue == "" || fimValue == "" || passoValue == "" || inicioValue >= fimValue){
-        window.alert('Error');
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert("Error");
+    }else{
+        res.innerHTML = 'Resultado: ';
+
+        for(var i = inicioValue; i <= fimValue; i+=passoValue){
+           res.innerHTML += `${i}`
+    
     }
 
-    res.innerHTML = "Resultado: ";
-    if(inicioValue <= fimValue){
-        for(var i = inicioValue; inicioValue <= fimValue; i+= passoValue){
-            res.innerHTML += `${i}`;
-           }
-    }else{
-        for(var i = inicioValue; inicioValue >= fimValue; i-= passoValue){
-            res.innerHTML += `${i}`;
-        }
+   /* var inicioValue = Number(inicio.value);
+    var fimValue = Number(fim.value);
+    var passoValue = Number(passo.value);
+
+      if(passoValue <= 0){
+        window.alert("Passo invalido! Considerando passo igual a 1.");
+        passoValue = 1;
     }
    
-  
+    if(inicioValue == 0 || fimValue == 0){
+        res.innerHTML = "Os dados estao incorretos tente novamente.";
+    }
+    
+    if(inicioValue <= fimValue){
+        res.innerHTML = "Resultado: ";
+        for(var i = inicioValue; inicioValue <= fimValue; i+= passoValue){
+        res.innerHTML += `${i}`;
+        }
+    }*/  
+    }
 }
