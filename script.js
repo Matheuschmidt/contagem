5121e6c5b2ae3bc9eab860d469b9159562ea7ec5
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-14 03:20:07 -0300

    corrigido para o meu formato anterior

--- script.js
+++ script.js
@@ -4,38 +4,17 @@ function contar(){
     var passo = document.querySelector('#txtpasso');
     var res = document.querySelector('#txtres');
 
-    var inicioValue = Number(inicio.value);
+
+   var inicioValue = Number(inicio.value);
     var fimValue = Number(fim.value);
     var passoValue = Number(passo.value);
 
-    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert("Error");
+      if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert("Erro");
     }else{
-        res.innerHTML = 'Resultado: ';
-
-        for(var i = inicioValue; i <= fimValue; i+=passoValue){
-           res.innerHTML += `${i}`
-    
-    }
-
-   /* var inicioValue = Number(inicio.value);
-    var fimValue = Number(fim.value);
-    var passoValue = Number(passo.value);
-
-      if(passoValue <= 0){
-        window.alert("Passo invalido! Considerando passo igual a 1.");
-        passoValue = 1;
-    }
-   
-    if(inicioValue == 0 || fimValue == 0){
-        res.innerHTML = "Os dados estao incorretos tente novamente.";
-    }
-    
-    if(inicioValue <= fimValue){
         res.innerHTML = "Resultado: ";
-        for(var i = inicioValue; inicioValue <= fimValue; i+= passoValue){
+        for(var i = inicioValue; i <= fimValue; i+= passoValue){
         res.innerHTML += `${i}`;
         }
-    }*/  
     }
 }
