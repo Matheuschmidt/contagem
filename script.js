e161933d87dadc822824240222e54cb726acda72
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-14 03:44:47 -0300

    projeto terminado

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ function contar(){
     var passo = document.querySelector('#txtpasso');
     var res = document.querySelector('#txtres');
 
-
-   var inicioValue = Number(inicio.value);
+    var inicioValue = Number(inicio.value);
     var fimValue = Number(fim.value);
     var passoValue = Number(passo.value);
+    
+    
 
-      if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert("Erro");
-    }else{
-        res.innerHTML = "Resultado: ";
-        for(var i = inicioValue; i <= fimValue; i+= passoValue){
-        res.innerHTML += `${i}`;
+    if(inicio.value.length == 0 || fim.value.length == 0){
+        res.innerHTML = "Impossivel contar.";  
+       // window.alert("[Erro] Faltam Dados.");
+        }else{
+        res.innerHTML = "Resultado: <br>";
+        if(passoValue <= 0){
+            alert("Passo Invalido. Considerando Passo igual a 1.");
+            passoValue = 1;
         }
+        if(inicioValue < fimValue){ //CONTAGEM CRESCENTE
+            for(var i = inicioValue; i <= fimValue; i+= passoValue){
+                res.innerHTML += ` ${i}`;
+            }
+        }else{ // CONTAGEM REGRESSIVA
+            for(var i = inicioValue; i >= fimValue; i-= passoValue){
+                res.innerHTML += ` ${i}`;
+    
+            }
+        } 
     }
 }
